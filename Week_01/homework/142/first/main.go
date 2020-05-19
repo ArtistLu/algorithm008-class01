55d1a09309b95c5e990bbd5924006ee0bf46822b
zhanglu6 <zhanglu6@doumi.com>
2020-05-19 22:50:32 +0800

    week01复习

---
+++ Week_01/homework/142/first/main.go
@@ -0,0 +1,32 @@
+package main
+
+func main() {
+
+}
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	fast, slow := head, head
+
+	for fast != nil && fast.Next != nil {
+		fast, slow = fast.Next.Next, slow.Next
+		if fast == slow {
+			break
+		}
+	}
+
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+
+	fast = head
+	for fast != slow {
+		fast, slow = fast.Next, slow.Next
+	}
+
+	return fast
+}
